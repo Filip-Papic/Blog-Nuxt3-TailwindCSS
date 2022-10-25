3221d8d654af6d63e7ea1563c4abd5b2c3e812b9
Filip-Papic <filippapic97@gmail.com>
2022-10-25 23:39:50 +0200

    init

--- app.vue
+++ app.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
+  <div class="mx-4 md:mx-48">
     <Header />
     <NuxtPage />
   </div>
 </template>
+
+<style>
+body {
+  background-color: #1C1C1C;
+  color: #FFE002;
+}
+</style>

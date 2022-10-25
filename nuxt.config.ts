19db574b39780e6bf72517327ac13d812e3e541d
Filip-Papic <filippapic97@gmail.com>
2022-10-25 23:19:46 +0200

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,20 @@
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  modules: [
+    "@nuxtjs/tailwindcss",
+    [
+      "@pinia/nuxt",
+      {
+        autoImports: ["defineStore", "acceptHMRUpdate"],
+      },
+    ],
+    "nuxt-icon",
+    "@nuxtjs/color-mode",
+  ],
+  imports: {
+    dirs: ["stores"],
+  },
+  build: {
+    transpile: ["@headlessui/vue"],
+  },
+});
